cfa69c8a2c73b39dd9f6f42dda639369343d4266
flynnjrd <36885061+flynnjrd@users.noreply.github.com>
2018-02-27 20:27:38 -0600

    Add files via upload

--- MonteCarlo.m
+++ MonteCarlo.m
@@ -1,29 +1,42 @@
 clear all; clc;
-% n= number of points to run in Monte Carlo
-% A sum of points inside the circle
+% n= number of points to run in Monte Carlo (MC)
+% a sum of points inside the circle
 % x and y randomized values within the square/circle
 n=12000;
-A=0;
+a=0;
 x=rand(1,n);
 y=rand(1,n);
-%loop, increments for Monte Carlo 
+%matrix to store 10 runs of the MC
+B = cell(1,10);
+
+%loop to run MC 10 times, take average of values
 for j = 1:1:10;   
+    %loop, increments for MC
     for k=1:1:n;
-        
-        %pie=Monte Carlo pi estimation
-        pie=4*A/k; 
-        %rounded values of "real" pi and Monte Carlo pi
-        piR= round(pi,2);
-        pieR= round(pie,2);
-        
+                        
         if (x(k)^2)+(y(k)^2)<=1
-            A=A+1;
+            a=a+1;
         else 
-            A=A+0;
-        end
-        
+            a=a+0;
+        end             
     end
+     %pie=MC pi estimation
+      pie=4*a/n;
+    %assign MC value to matrix
+    B(1,j) = pie
+end
+
+ S = sum(B)
+ avg = S/10
+ 
+ %rounded values of "real" pi and averaged MC pi
+        piR= round(pi,2)
+        pieR= round(avg,2)
+ 
+ 
+
     
+
        
             
  
