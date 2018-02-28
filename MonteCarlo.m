811f0b7e02e9f5c946f32c71dc5d6d954ed464b3
flynnjrd <36885061+flynnjrd@users.noreply.github.com>
2018-02-28 13:20:00 -0600

    New final
    Realized I was taking the incorrect approach to taking average values. I was attempted to see statistically how high n needed to be to guarantee the desired precision 100% of the time. Realized though that this required extensive computing power and time. Went back to recording number of points when the estimated value first equals the true pi value and then taking the average of that

--- MonteCarlo.m
+++ MonteCarlo.m
@@ -2,47 +2,62 @@ clear all; clc;
 format long
 % n= number of points to run in Monte Carlo (MC)
 % a sum of points inside the circle
+% decimal precision
 % x and y randomized values within the square/circle
-n=750000000;
-b=5;
-x=double(rand(1,n));
-y=double(rand(1,n));
-%matrix to store b runs of the MC
+n=100000;
+b=100;
+d=2;
+
+%matrix to store number of points each run of MC to reach
+%desired precision, d
 B = 1:b;
 
 %loop to run MC 10 times, take average of values
-for j = 1:1:b;   
+ for j = 1:1:b
+     %x & y placed here to regenerate new values for each iteration
+     %a placed here to reset circle area value for each iteration
+     x=single(rand(1,n));
+     y=single(rand(1,n));
+     a=0;
     %loop, increments for MC
-    %a placed here to reset a value for each iteration
-    a=0;
-    for k=1:1:n;
+    
+ 
+    for k=1:1:n
+      r=(x(k)^2)+(y(k)^2);
+     
+            if  r<=1
+                a=a+1;
+            else 
+                a=a+0;
+            end 
+             %pie=MC pi estimation
+              pie=single(4*a/k);
+
+             %rounded values of "real" pi and averaged MC pi
+              piR= single(round(pi,d));
+              pieR= single(round(pie,d));
+             %Created dif to calculate the difference between the two pis
+             %Was checking if piR==pieR but the condition was not
+             %triggering. Appeared to be some rounding error
+              Dif=(10^d)*abs(piR-pieR);
                         
-        if (x(k)^2)+(y(k)^2)<=1
-            a=a+1;
-        else 
-            a=a+0;
-        end             
-    end
-     %pie=MC pi estimation
-      pie=double(4*a/n);
-    %assign MC value to matrix
-    B(1,j) = pie;
-end
+           if Dif<1
+            %assign k value to matrix to take average
+            B(1,j) = k;
+             disp(piR)
+             disp(pieR)
+             disp(k)
+            break      
+           end     
+    end    
+ end
 
 %sum and avg all of the MC runs to get an avg MC pi
-S = sum(B);
-avg = S/b
+ disp(B)
+ S = sum(B);
+ avg = S/b
 
-%rounded values of "real" pi and averaged MC pi
-piR= round(pi,4)
-pieR= round(avg,4)
 
-if pieR==piR
-    
-    disp('Yes! Its a match')
-else
-    disp('Nope')
-end
 
  
  
